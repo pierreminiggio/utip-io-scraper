17c78f74c056ed39d0a91efca166071f4ed80b5a
Pierre MINIGGIO <pierre@miniggiodev.fr>
2020-03-18 23:26:55 +0100

    somehow half working

--- money.js
+++ money.js
@@ -2,18 +2,46 @@ const puppeteer = require('puppeteer')
 
 const rackupMoney = async (slug) => {
 
-    const browser = await puppeteer.launch({ headless: false })
+    const browser = await puppeteer.launch({ headless: true })
+
+    let message = 'Il y a eu une erreur'
 
     try {
         const page = await browser.newPage()
 
-        await page.goto('https://utip.io/' + slug)
+        page.goto('https://utip.io/' + slug)
+
+        console.log('Let\'s find an ad !')
+
+        await page.waitFor(20000)
+    
+        await page.click(
+            'img[src="https://cdn.utip.eu/build/images/play_white.2549a3e4.svg"]',
+        )
+
+        await page.waitFor(60000)
+
+        const success = await page.evaluate(() => {
+            return document.querySelector('button-no-style button-donation.button-donation-mobile') !== null
+        })
+
+        if (success) {
+            await page.click(
+                '.yellow-btn.button-no-style',
+            )
+
+            console.log('Money credited !')
+
+            await page.waitFor(10000)
+        } else {
+            console.log('No ad :\'(')
+        }
 
     } catch (e) {
-        console.log(e)
+        console.error(e)
     }
 
-    //browser.close()
+    browser.close()
 }
 
 function letsGo () {
@@ -21,8 +49,10 @@ function letsGo () {
         console.warn('Use this program like this: "node money.js <your-utip-slug>"')
     } else {
         const slug = process.argv[2]
-        console.log("\n\nLet's run ads on " + slug + "'s page :")
-        rackupMoney(slug)
+        console.log("\n\nLet's run an ad on " + slug + "'s page :")
+        rackupMoney(slug).then(() => {
+            letsGo()
+        })
     }
 }
 
